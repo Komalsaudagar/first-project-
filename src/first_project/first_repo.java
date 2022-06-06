0b690f5c5cf385dcfd4cf4f8de2aedbadb6eff0e
HP <you@example.com>
2022-06-07 00:26:09 +0530

    first project....

---
+++ src/first_project/first_repo.java
@@ -0,0 +1,17 @@
+package first_project;
+
+public class first_repo {
+
+	public static void main(String[] args) {
+		int num=5;
+		int factorial=1;
+		for(int i=1;i<=5;i++) {
+			factorial=factorial*i;
+			
+		}
+		System.out.println("factorial:"+factorial);
+	}
+	
+	}
+
+}
